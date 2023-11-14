b54e2c36ae9457d4a8435f24bad0682977b15b94
Maksim <maxus2004123@gmail.com>
2023-11-14 18:04:43 +0000

    put api server in a variable

--- common.js
+++ common.js
@@ -3,11 +3,13 @@ export { login, password, api}
 var login = localStorage.getItem('login')
 var password = localStorage.getItem('password')
 
+var api_server = 'vorota.servermaksa.tk/api'
+
 async function api(cmd, params) {
     if (login == null || password == null || login == '') {
         window.location = '/login/'
     }
-    let request = `/api/?cmd=${cmd}&login=${login}&password=${password}`
+    let request = `${api_server}/?cmd=${cmd}&login=${login}&password=${password}`
     for (let param in params) {
         request += `&${param}=${params[param]}`
     }
