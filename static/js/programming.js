8a9b2b8fb9c079e8b532c3c15d930bcf7a9b020b
max <m1doroshin@gmail.com>
2024-07-25 00:09:04 +0300

    improve ui

--- static/js/programming.js
+++ static/js/programming.js
@@ -2,14 +2,23 @@ import { api } from '/static/js/camera_api.js'
 
 window.onload = async function () {
     document.getElementById('copy_btn').onclick = startCopying
+    document.getElementById('button_select').onselect = onSelect
 }
 
-async function startCopying(){
+function onSelect() {
+    if (document.getElementById('button_select').value == "") {
+        document.getElementById('copy_btn').disabled = true;
+    } else {
+        document.getElementById('copy_btn').disabled = false;
+    }
+}
+
+async function startCopying() {
     let camera_id = sessionStorage.getItem("selected_camera")
     if (camera_id == undefined) camera_id = 0
     let camera = JSON.parse(localStorage.getItem('user_data')).cameras[camera_id]
 
-    let btn_id = prompt("введите номер кнопки")
-    let result = await api(camera.id,camera.key,'prog_btn', { id: btn_id }, 30000)
+    let btn_id = parseInt(document.getElementById('button_select').value) - 1
+    let result = await api(camera.id, camera.key, 'prog_btn', { id: btn_id }, 30000)
     alert(result)
 }
