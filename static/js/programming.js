3de95bdc496b404f3a2d4e65886432989569b63c
max <m1doroshin@gmail.com>
2024-07-24 23:43:10 +0300

    fix

--- static/js/programming.js
+++ static/js/programming.js
@@ -5,6 +5,9 @@ window.onload = async function () {
 }
 
 async function startCopying(){
+    let id = JSON.parse(localStorage.getItem('user_data')).cameras[camera_select.value].id
+    let key = JSON.parse(localStorage.getItem('user_data')).cameras[camera_select.value].key
+
     let btn_id = prompt("введите номер кнопки")
     let result = await api(id,key,'prog_btn', { id: btn_id }, 30000)
     alert(result)
