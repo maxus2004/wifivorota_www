9902493585fdc18c8f7c5668886f1eb24b60588e
max <m1doroshin@gmail.com>
2024-07-25 21:40:23 +0300

    added UI for remote type select

--- static/js/programming.js
+++ static/js/programming.js
@@ -3,10 +3,11 @@ import { api } from '/static/js/camera_api.js'
 window.onload = async function () {
     document.getElementById('copy_btn').onclick = startCopying
     document.getElementById('button_select').onchange = onSelect
+    document.getElementById('type_select').onchange = onSelect
 }
 
 function onSelect() {
-    if (document.getElementById('button_select').value == "") {
+    if (document.getElementById('button_select').value == "" || document.getElementById('type_select').value == "") {
         document.getElementById('copy_btn').disabled = true;
     } else {
         document.getElementById('copy_btn').disabled = false;
@@ -19,6 +20,7 @@ async function startCopying() {
     let camera = JSON.parse(localStorage.getItem('user_data')).cameras[camera_id]
 
     let btn_id = parseInt(document.getElementById('button_select').value) - 1
+    //TODO: support complex remotes
     let result = await api(camera.id, camera.key, 'prog_btn', { id: btn_id }, 30000)
     alert(result)
 }
